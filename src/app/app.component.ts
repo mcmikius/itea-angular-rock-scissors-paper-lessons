1481fbceb92b5cf8402ceb164e26cd1367d77ce6
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-08-06 13:18:37 +0300

    Add material library HTML first page Add game-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'itea-angular-rock-scissors-paper-lessons';
+  userName = '';
+  isFirstScreen = false;
+
+  auth() {
+    this.isFirstScreen = true;
+  }
 }
-
-
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule, MatInputModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule
+} from "@angular/material";
+import { GamePageComponent } from './components/game-page/game-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+      MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
