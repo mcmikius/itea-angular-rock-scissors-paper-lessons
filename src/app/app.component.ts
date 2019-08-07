bf03f717f2d775dbe8b776c86d73743f7b0a20f1
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-08-07 16:48:13 +0300

    add functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,46 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Rock Paper Scissors Lizard Spock';
   desc = 'The Rock Paper Scissors Game plus Lizard Spock';
+  weapons = [
+    'rock',
+    'paper',
+    'scissors',
+    'spock',
+    'lizard'
+  ];
+  playerWeapon = 0;
+  computerWeapon = 0;
+  scores = [0 , 0];
+  result = 4;
+  resultStrings = [
+    'TEI',
+    'PLAYER WIN',
+    'COMPUTER WIN',
+    ''
+  ];
+  resultMessage = 'AWAITING GAME COMMENCEMENT';
+
+  chooseWeapon( weapon: number): void {
+    this.playerWeapon = weapon;
+    console.log(this.playerWeapon);
+    this.computerWeapon =  Math.floor(Math.random() * 3 );
+    console.log(this.computerWeapon);
+    this.calculateResult();
+  }
+  calculateResult(): void {
+    if ( this.playerWeapon === this.computerWeapon) {
+      this.result = 0;
+    } else if (( (this.playerWeapon - this.computerWeapon + 5) % 5) === 1) {
+      this.scores[0] += 1;
+      this.result = 1;
+    } else if (( (this.playerWeapon - this.computerWeapon + 5) % 5) === 3) {
+      this.scores[0] += 1;
+      this.result = 1;
+    } else {
+      this.scores[1] += 1;
+      this.result = 2;
+    }
+    this.resultMessage = 'Computer chooses ' + this.weapons[this.computerWeapon] + ' ||| '  + this.resultStrings[this.result];
+  }
+
 }
