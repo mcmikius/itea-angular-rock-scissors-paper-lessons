1c1b511f5b6eb3e084e2e673f56f4f82676d2a68
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-08-07 15:39:30 +0300

    add mat toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'itea-angular-rspls-lessons';
+  title = 'Rock Paper Scissors Lizard Spock';
 }
